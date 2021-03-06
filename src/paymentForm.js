fe4adbc8b36d5e85a1919f5eac4d5a89b2085a00
pmohsin0721 <pmohsin218>
2021-03-06 20:43:10 +0530

    first commit

---
+++ src/paymentForm.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./paymentForm.css";
+
+const paymentForm = () => {
+  return (
+    <>
+      <div>
+        <div className="form-1"></div>
+      </div>
+    </>
+  );
+};
+
+export default paymentForm;
